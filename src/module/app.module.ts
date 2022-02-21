eda7cb1e9603a5ad1d71fc0ec0f345cfa5deb154
brendson <brendson.ne@gmail.com>
2022-02-21 12:24:28 -0300

    feat: create config .env

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 import { AppController } from '../controller/app.controller';
 import { AppService } from '../service/app.service';
@@ -8,12 +9,13 @@ import { LivrosService } from '../service/livros.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: '1234',
+      username: process.env.USUARIO_BANCO_DADOS,
+      password: process.env.SENHA_BANCO_DADOS,
       database: 'livraria',
       autoLoadModels: true,
       synchronize: true,
