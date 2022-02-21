786a71747b9887bb6a376e830658d4487057598c
brendson <brendson.ne@gmail.com>
2022-02-21 11:38:20 -0300

    feat: integration database

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 import { AppController } from '../controller/app.controller';
 import { AppService } from '../service/app.service';
-import { ProdutosController } from '../controller/produtos.controller';
-import { ProdutosService } from 'src/service/produtos.service';
+import { Livro } from '../schema/livro.model';
+import { LivrosController } from '../controller/livros.controller';
+import { LivrosService } from '../service/livros.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProdutosController],
-  providers: [AppService, ProdutosService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro]),
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
 })
 export class AppModule {}
